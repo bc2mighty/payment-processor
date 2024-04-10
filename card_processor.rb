2c378da1b2ce196d8c65a5566dcacbcff5d10d98
bc2mighty <akinadetunmise8@gmail.com>
2024-04-10 19:40:33 +0100

    feat: added card type and card number validation method to card_processor

--- card_processor.rb
+++ card_processor.rb
@@ -1,11 +1,12 @@
 class CardProcessor
-    def initialize(card_number, ccv, owner_name, expiration_date, zip_code, amount)
+    def initialize(card_number, ccv, owner_name, expiration_date, zip_code, amount, card_type)
       @card_number = card_number
       @ccv = ccv
       @owner_name = owner_name
       @expiration_date = expiration_date
       @zip_code = zip_code
       @amount = amount
+      @card_type = card_type
     end
   
     def card_expired?
@@ -18,6 +19,14 @@ class CardProcessor
       else
         false
       end
+    end
+  
+    def is_valid_card_number?
+      return false unless ['American Express', 'Mastercard', 'Visa'].include?(@card_type)
+      return false if @card_type == 'American Express' && @card_number.size != 15
+      return false if %w[Mastercard Visa].include?(@card_type) && @card_number.size != 16
+  
+      true
     end
   
     # In this exercise we can assume that if everything is otherwise valid the
