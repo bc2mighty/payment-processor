bd01183787afe29b7844abd5b89273b04cebf3ee
bc2mighty <akinadetunmise8@gmail.com>
2024-04-10 19:52:11 +0100

    feat: added total payments, total dollar amount processed, and amount by card type reports

--- report.rb
+++ report.rb
@@ -5,4 +5,25 @@ class Report
     @payments = payments
     @amount_by_card_type = {}
   end
+
+  def total_payments
+    @payments.count
+  end
+
+  def total_dollar_amount_processed
+    @payments.inject(0.0) { |total, arr| total + cent_to_dollar(arr[5]) }
+  end
+
+  def get_amount_by_card_type
+    @payments.each do |card|
+      amount = cent_to_dollar(card[5])
+      if @amount_by_card_type.key?(card[6])
+        @amount_by_card_type[card[6]] += amount
+      else
+        @amount_by_card_type[card[6]] =
+          amount
+      end
+    end
+    @amount_by_card_type
+  end
 end
