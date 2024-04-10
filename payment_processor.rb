e7bf97effbdfcee5ccc6b6ee830cf2930aa9cf24
bc2mighty <akinadetunmise8@gmail.com>
2024-04-10 19:47:54 +0100

    feat: check if card number is valid before processing

--- payment_processor.rb
+++ payment_processor.rb
@@ -1,60 +1,29 @@
 require "csv"
-
-class CardProcessor
-  def initialize(card_number, ccv, owner_name, expiration_date, zip_code, amount)
-    @card_number = card_number
-    @ccv = ccv
-    @owner_name = owner_name
-    @expiration_date =  expiration_date
-    @zip_code = zip_code
-    @amount = amount
-  end
-
-  def card_expired?
-    month,year = @expiration_date.split("/").map(&:to_i)
-
-    return true if Time.now.year > year.to_i
-    if Time.now.year == year
-      Time.now.month <= month
-    else
-      false
-    end
-  end
-
-  # In this exercise we can assume that if everything is otherwise valid the
-  # payment will process successfully
-  def process!
-    true
-  end
-end
+require './card_processor'
+require './report'
 
 class PaymentProcessor
   def self.process(input)
     @input = input
-
-    @processed_cards = []
+    @processed_payments = []
 
     cards_to_try.each do |card|
       processor = CardProcessor.new(
-        card[1], card[2], card[0], card[4], card[3], card[5]
+        card[1], card[2], card[0], card[4], card[3], card[5], card[6]
       )
-      next if processor.card_expired?
-      @processed_cards << processor.process!
+      next if processor.card_expired? || !processor.is_valid_card_number?
+
+      processed = processor.process!
+      @processed_payments << card if processed
     end
-
-    report
-  end
-
-  def self.report
-    "Total payments: #{total_payments}"
   end
 
   def self.cards_to_try
     @input[1..-1]
   end
 
-  def self.total_payments
-    @processed_cards.count { |status| status == true }
+  def self.get_processed_payments
+    @processed_payments
   end
 end
 
